4a5326fbedd1e63289f69a03f7ab5e0665274578
Tanya <vodilova@mail.ru>
2021-07-30 12:17:27 +0300

    complicated task

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ let money = 50000,
     
 
     // alert('Привет, мир!');
-    console.log('Hello, world');
-    console.log(typeof money);
-    console.log(typeof income);
-    console.log(typeof deposit);
-    console.log(addExpenses.length);
-    console.log('Период равен '+ period + ' месяцев, ' + 'Цель заработать ' + money + ' рублей');
-    console.log(addExpenses.toLowerCase());
-    console.log(addExpenses.toLowerCase().split(','));
-    console.log('budgetDay : ', budgetDay );
+    // console.log('Hello, world');
+    // console.log(typeof money);
+    // console.log(typeof income);
+    // console.log(typeof deposit);
+    // console.log(addExpenses.length);
+    // console.log('Период равен '+ period + ' месяцев, ' + 'Цель заработать ' + money + ' рублей');
+    // console.log(addExpenses.toLowerCase());
+    // console.log(addExpenses.toLowerCase().split(','));
+    // console.log('budgetDay : ', budgetDay );
 
   
