93a1281109595fffc3e036063cbb1d3e01271cb2
Tanya <vodilova@mail.ru>
2021-07-30 08:36:17 +0300

    Lesson 2

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-let money, 
-    income, 
-    addExpenses, 
-    deposit, 
-    mission, 
-    period;
+let money = 50000, 
+    income = 'Фриланс', 
+    addExpenses = 'Связь, такси, коммуналка', 
+    deposit = false, 
+    mission = 200000, 
+    period = 12
+    budgetDay = money/30;
+    
 
-    alert('Привет, мир!');
+    // alert('Привет, мир!');
     console.log('Hello, world');
+    console.log(typeof money);
+    console.log(typeof income);
+    console.log(typeof deposit);
+    console.log(addExpenses.length);
+    console.log('Период равен '+ period + ' месяцев, ' + 'Цель заработать ' + money + ' рублей');
+    console.log(addExpenses.toLowerCase());
+    console.log(addExpenses.toLowerCase().split(','));
+    console.log('budgetDay : ', budgetDay );
+
+  
