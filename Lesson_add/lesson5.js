62f67f07573fa93defce767115d35ff8acfeaf37
Tanya <vodilova@mail.ru>
2021-08-04 14:52:10 +0300

    Lesson 5

---
+++ Lesson_add/lesson5.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// 1
+let arr = ['12', '58777', '43299','55378', '423', '99335', '287924'];
+
+for (let i = 0; i < arr.length; i++) {
+  let num = String(arr[i]);  /* получаем каждый элемент массива последовательно*/
+  // let char = num[0];  /* первый способ получить первый элемент*/  
+  num.charAt(1); /*второй способ*/
+  if (num.charAt(0) == 2  || num.charAt(0) == 4) {
+    console.log(num);
+  }
+}
+
+// 2 Вывод простых чисел
+
+// проверяю простое ли число через остаток при делении  
+let n = 100;  
+function isPrime(n) {
+  for (let i = 2; i < n; i++) {
+    if ( n % i == 0) return false;
+  }
+  return true;
+}
+showPrimes(n);
+
+//  вывожу только простые перескакивая через остальные 
+function showPrimes(n) {
+
+    for (let i = 2; i < n; i++) {
+      if (!isPrime(i)) continue;
+  
+      console.log(i + ' Делители этого числа: 1 и ' + i);  // простое
+    }
+  }
+
