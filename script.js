79546348f11e358af01c51f86fc260a7d3c65168
Tanya <vodilova@mail.ru>
2021-08-09 21:12:13 +0300

    Lesson09

--- script.js
+++ script.js
@@ -1,6 +1,58 @@
 'use strict';
 
-let money;
+
+
+let money = document.querySelector('.salary-amount'), 
+    itemIncome = document.querySelector('.income-items> .income-title '),
+    incomeAmount = document.querySelector('.income-amount'), 
+    btnIncome = document.getElementsByTagName('button')[0],
+    btnExpenses= document.getElementsByTagName('button')[1],
+    additionalIncomeItem = document.querySelectorAll(' .additional_income-item'),
+    btnStart = document.getElementById('start'),
+    depositСheck = document.querySelector('#deposit-check'),
+    budgetMonth = document.getElementsByClassName('budget_month-value')[0],
+    budgetDay = document.getElementsByClassName('budget_day-value')[0],
+    expensesMonth = document.getElementsByClassName('expenses_month-value')[0],
+    additionalIncome = document.getElementsByClassName('additional_income-value')[0],
+    additionalExpenses = document.getElementsByClassName('additional_expenses-value')[0],
+    incomePeriod = document.getElementsByClassName('income_period-value')[0],
+    targetMonth = document.getElementsByClassName('target_month-value')[0],
+    expenses = document.querySelector('.expenses-items> .expenses-title'),
+    expensesAmount= document.querySelector('.expenses-amount'),
+    additionalExpensesItem = document.querySelector('.additional_expenses-item'),
+    targetAmount = document.querySelector('.target-amount'),
+    periodAmount= document.querySelector('[type="range"]');
+ 
+   
+        
+ 
+// let money;
+
+console.log(money);
+console.log(itemIncome);
+console.log(incomeAmount);
+console.log(btnIncome);
+console.log(btnExpenses);
+console.log(additionalIncomeItem);
+console.log(btnStart);
+console.log(depositСheck);
+console.log(budgetMonth);
+console.log(budgetDay);
+console.log(expensesMonth);
+console.log(additionalIncome);
+console.log(additionalExpenses);
+console.log(incomePeriod);
+console.log(targetMonth);
+console.log(expenses);
+console.log(expensesAmount);
+console.log(additionalExpensesItem);
+console.log(targetAmount);
+console.log(periodAmount);
+
+
+
+
+
 // проверка на число
 let isNumber = function(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
@@ -158,7 +210,7 @@ let appData = {
   console.log(appData.percentDeposit, appData.moneyDeposit,appData.calcSaveMoney());
  
   for (let key in appData){
-    console.log('Hаша программа включает в себя данные:: ' + key + ' значение: ' + appData[key]);
+    console.log('Hаша программа включает в себя данные: ' + key + ' значение: ' + appData[key]);
     
    }
     
